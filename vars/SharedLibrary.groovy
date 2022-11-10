7346d02d66d30e9bca254ec0360ad4dc311d0b1c
zulfi1098 <zulfi1098@yahoo.com>
2022-11-11 00:40:43 +0400

    f

--- vars/SharedLibrary.groovy
+++ vars/SharedLibrary.groovy
@@ -16,3 +16,22 @@ def builders = [:]
   }
   }
 }
+
+def call1() {
+stage {
+    
+def allModules = ['module1', 'module2', 'module3', 'module4', 'module5']
+def builders = [:]
+  allModules.each() {
+    builders[it] = {
+                        stage(it){
+                                 script { 
+                                            echo 'Call of Shared Library paramater less'
+                                        }
+                        }
+                    }
+                                  
+                    parallel builders
+  }
+  }
+}
