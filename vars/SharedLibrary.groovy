46d5f2141ed9bc929aa74979e61df9864f0abe27
zulfi1098 <zulfi1098@yahoo.com>
2022-11-11 00:44:29 +0400

    f

--- vars/SharedLibrary.groovy
+++ vars/SharedLibrary.groovy
@@ -1,37 +1,39 @@
 def call() {
-stage {
+    stage {
     
-def allModules = ['module1', 'module2', 'module3', 'module4', 'module5']
-def builders = [:]
-  allModules.each() {
-    builders[it] = {
-                        stage(it){
-                                 script { 
-                                            echo 'Call of Shared Library paramater less'
-                                        }
-                        }
-                    }
+        def allModules = ['module1', 'module2', 'module3', 'module4', 'module5']
+        def builders = [:]
+          allModules.each() {
+                builders[it] = {
+                                    stage(it){
+                                             script { 
+                                                        echo 'Call of Shared Library paramater less'
+                                                    }
+                                    }
+                                }
                                   
-                    parallel builders
-  }
-  }
+                               
+          }
+           parallel builders
+    }
 }
 
 def call1() {
-stage {
+    stage {
     
-def allModules = ['module1', 'module2', 'module3', 'module4', 'module5']
-def builders = [:]
-  allModules.each() {
-    builders[it] = {
-                        stage(it){
-                                 script { 
-                                            echo 'Call of Shared Library paramater less'
-                                        }
-                        }
-                    }
+    def allModules = ['module1', 'module2', 'module3', 'module4', 'module5']
+    def builders = [:]
+          allModules.each() {
+            builders[it] = {
+                                stage(it){
+                                         script { 
+                                                    echo 'Call of Shared Library paramater less'
+                                                }
+                                }
+                            }
                                   
-                    parallel builders
-  }
-  }
+                           
+          }
+           parallel builders
+      }
 }
